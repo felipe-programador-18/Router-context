eaef11b6d087854d60b3f77cd85633689e3ed2fe
Felipe <machadofelipe2016@outlook.com>
2022-07-02 12:30:48 -0300

    c4-adding someone router inside page principles

--- context/src/App.js
+++ context/src/App.js
@@ -1,12 +1,23 @@
-
+import { Routes , Route } from 'react-router-dom';
 import './App.css';
+import Contato from './Component/Contato';
+import Home from './Component/Home';
+import Nav from './Component/Navbar';
+import Sobre from './Component/Sobre';
+import Notfound from './page/Notfound';
 
 function App() {
-  return (
-    <div className="App">
-    <h1>practice more and more always </h1>
-    </div>
-  );
+  return ( <>
+    <Nav/>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/sobre' element={ <Sobre/> } />
+      <Route  path='/contato' element={<Contato/>}  />
+      <Route path='*' element={ <Notfound/>} />
+      <Route/>
+    </Routes>
+
+</>  );
 }
 
 export default App;
