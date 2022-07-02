0f99f52d293a5d41210a6d890784c3fc05e422d1
Felipe <machadofelipe2016@outlook.com>
2022-07-02 16:25:10 -0300

    c5-defined and create all context of aplicatoin

---
+++ context/src/Context/counter.js
@@ -0,0 +1,15 @@
+import { createContext, useState } from "react";
+
+
+//theoretical i have create context of another way!!
+export const CounterCountext = createContext();
+
+export const CounterManageProvider = ({children}) => {
+    const [counter, setCounter] = useState(12)
+    const [phase,setPhase] = useState("adding new value about state!!")
+    return (  
+      <CounterCountext.Provider value={{counter,phase, setCounter}} >
+       {children}
+      </CounterCountext.Provider>
+    )
+}
