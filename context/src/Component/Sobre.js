bde7fdf7062ca16385441c00077027b9f4c773a3
Felipe <machadofelipe2016@outlook.com>
2022-07-02 16:43:47 -0300

    c7-create file about and passed it context api about her

--- context/src/Component/Sobre.js
+++ context/src/Component/Sobre.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import './component.css'
-
+import { CounterCountext } from '../Context/counter'
 
 const Sobre = () => {
+  const {counter,phase, setCounter} = useContext(CounterCountext)
+
+  const IncreaseCounter = () => {
+    setCounter(counter+1)  
+  }
+  
+  const Decrement = () => {
+      if(counter > 0){
+          setCounter(counter-1)
+      }
+  }
+
     return (<div className='information'>
                  
          <h1>Information about how create new code with more effectvily</h1>
-            
+         <button onClick={IncreaseCounter} >+</button>
+         <button onClick={Decrement} >-</button>
+            {counter}
+            <br/>
+            {phase}
     </div>)
 }
 
