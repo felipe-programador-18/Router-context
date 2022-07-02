d3e4b665dbe4c6dc91434c917cb2973b84ebe016
Felipe <machadofelipe2016@outlook.com>
2022-07-02 18:07:46 -0300

    c10-create personalities hoock

---
+++ context/src/hoock/useCounterCounter.js
@@ -0,0 +1,14 @@
+import { useContext } from "react"
+import { CounterCountext } from "../Context/counter"
+
+// this turned hoock personalites to together work about contextapi!!
+
+export const useCounterHook = () => {
+  const context = useContext(CounterCountext)
+
+  if(!context){
+    console.log('context not found!!!')
+  }   
+
+  return context
+}
