82827d74038c2d1d08831f1cc349f779368d5e3f
Felipe <machadofelipe2016@outlook.com>
2022-07-02 16:45:12 -0300

    c9-adding context inside home

--- context/src/Component/Home.js
+++ context/src/Component/Home.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import "./component.css"
+import  {CounterCountext}  from '../Context/counter'
 const Home = () => {
+    const {counter,phase, setCounter} = useContext(CounterCountext)
     
-    return (<>
-    <h1 className='information' >Information about home and my products!!</h1>
+    const IncreaseCounter = () => {
+      setCounter(counter+1)  
+    }
+    
+    const Decrement = () => {
+        if(counter > 0){
+            setCounter(counter-1)
+        }
+    }
 
+
+    return (<>
+    <h1 className='information' >Information about home and my products!!</h1>{counter}
+     <button onClick={IncreaseCounter} >Increase</button>
+     <button onClick={Decrement} >Decrement</button>
     
+       <h2>  {phase} </h2>
     </>)
 }
 
