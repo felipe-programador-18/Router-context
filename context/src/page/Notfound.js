fbf1fb9015a9baaa569b5383b2bfb8ebe753033b
Felipe <machadofelipe2016@outlook.com>
2022-07-02 12:29:07 -0300

    create folder page to manage file notfond

---
+++ context/src/page/Notfound.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Styles.css'
+
+const Notfound = () => {
+    return (<>
+       <h1 className='center' > not found here 404 ...... </h1>
+    </>)
+}
+
+export default Notfound
