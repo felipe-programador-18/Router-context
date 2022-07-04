1744bad5493057dc2f49cc5fdcf80e870414780f
Felipe <machadofelipe2016@outlook.com>
2022-07-04 13:58:45 -0300

    c23-practice little bit about adding value of context in another file of project

--- context/src/Component/Contato.js
+++ context/src/Component/Contato.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import "./component.css"
+import { useTitleCreateColor } from '../hoock/useTitleColor'
+
 const Contato = () => {
+   const{color} = useTitleCreateColor()
+    
     return (<>
     <div className='information' >
        <p>information about my how contact my!!!</p>
        <small>hi is testing old tag!!</small>
+       <h1 style={{color:color}} >Change color now, how is state !!</h1>
     </div>
     </>)
 }
