10410546ae8c7ea3c7c1153297056764008da221
Felipe <machadofelipe2016@outlook.com>
2022-07-04 13:56:05 -0300

    c20-adding switch inside function to trade color

--- context/src/Context/TitleColor.js
+++ context/src/Context/TitleColor.js
@@ -6,16 +6,24 @@ export const TitleChangeColor = createContext()
 export const titleColorReducer = (state, action) => {
 // i can works and did with if and else, but this case i'm working with
 //switch
-
-   // return state
+   switch(action.type){
+    case "RED": 
+     return {...state, color:'red'}
+    case "BLUE":
+     return {...state, color:'blue'}
+    case "GREEN":
+     return {...state, color:'green'}
+    default:
+        return state
+   }
 }
 
 export const CreatContexProv = ({children}) =>{
    
-    const [state, dispacth] = useReducer(titleColorReducer, {color:"red"})
+    const [state, dispatch] = useReducer(titleColorReducer, {color:"gray"})
 
     return(
-     <TitleChangeColor.Provider value={{...state}} >
+     <TitleChangeColor.Provider value={{...state, dispatch}} >
         {children}
     </TitleChangeColor.Provider>  )
 }
