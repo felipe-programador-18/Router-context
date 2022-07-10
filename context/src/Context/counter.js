e6935d7426fcb295e6b6e9ee9f4dd7fd61b1b4e3
Felipe <machadofelipe2016@outlook.com>
2022-07-10 14:22:47 -0300

    c24-adding more code to practice about context api

--- context/src/Context/counter.js
+++ context/src/Context/counter.js
@@ -1,11 +1,11 @@
 import { createContext, useState } from "react";
 
-
 //theoretical i have create context of another way!!
 export const CounterCountext = createContext();
 
 export const CounterManageProvider = ({children}) => {
     const [counter, setCounter] = useState(12)
+    console.log('test counter', counter)
     const [phase,setPhase] = useState("adding new value about state!!")
     return (  
       <CounterCountext.Provider value={{counter,phase, setCounter, setPhase}} >
